c71f35bea3d6511f7415b760cb1a8866db596a9c
Devenderreddypingili <133239440+Devenderreddypingili@users.noreply.github.com>
2023-06-06 14:40:17 -0500

    Update TripList.vue

--- src/views/TripList.vue
+++ src/views/TripList.vue
@@ -26,46 +26,36 @@ onMounted(async () => {
 
 async function getTrips() {
   user.value = JSON.parse(localStorage.getItem("user"));
-  if (user.value !== null && user.value.id !== null) {
-    await TripServices.getTripsByUserId(user.value.id)
-      .then((response) => {
-        trips.value = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        snackbar.value.value = true;
-        snackbar.value.color = "error";
-        snackbar.value.text = error.response.data.message;
-      });
-  } else {
-    await TripServices.getTrips()
-      .then((response) => {
-        trips.value = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        snackbar.value.value = true;
-        snackbar.value.color = "error";
-        snackbar.value.text = error.response.data.message;
-      });
+  try {
+    if (user.value !== null && user.value.id !== null) {
+      const response = await TripServices.getTripsByUserId(user.value.id);
+      trips.value = response.data;
+    } else {
+      const response = await TripServices.getTrips();
+      trips.value = response.data;
+    }
+  } catch (error) {
+    console.log(error);
+    snackbar.value.value = true;
+    snackbar.value.color = "error";
+    snackbar.value.text = error.response.data.message;
   }
 }
 
 async function addTrip() {
   isAdd.value = false;
   newTrip.value.userId = user.value.id;
-  await TripServices.addTrip(newTrip.value)
-    .then(() => {
-      snackbar.value.value = true;
-      snackbar.value.color = "green";
-      snackbar.value.text = `${newTrip.value.name} added successfully!`;
-    })
-    .catch((error) => {
-      console.log(error);
-      snackbar.value.value = true;
-      snackbar.value.color = "error";
-      snackbar.value.text = error.response.data.message;
-    });
+  try {
+    await TripServices.addTrip(newTrip.value);
+    snackbar.value.value = true;
+    snackbar.value.color = "success";
+    snackbar.value.text = `${newTrip.value.name} added successfully!`;
+  } catch (error) {
+    console.log(error);
+    snackbar.value.value = true;
+    snackbar.value.color = "error";
+    snackbar.value.text = error.response.data.message;
+  }
   await getTrips();
 }
 
@@ -86,74 +76,43 @@ function closeSnackBar() {
   <v-container>
     <div id="body">
       <v-row align="center" class="mb-4">
-        <v-col cols="10"
-          ><v-card-title class="pl-0 text-h4 font-weight-bold"
-            >Trips
-          </v-card-title>
+        <v-col cols="10">
+          <v-card-title class="pl-0 text-h4 font-weight-bold">Trips</v-card-title>
         </v-col>
         <v-col class="d-flex justify-end" cols="2">
-          <v-btn v-if="user !== null" color="accent" @click="openAdd()"
-            >Add</v-btn
-          >
+          <v-btn v-if="user !== null" color="primary" @click="openAdd">Add</v-btn>
         </v-col>
       </v-row>
 
-      <TripCard
-        v-for="trip in trips"
-        :key="trip.id"
-        :trip="trip"
-        @deletedList="getLists()"
-      />
+      <TripCard v-for="trip in trips" :key="trip.id" :trip="trip" @deletedList="getLists" />
 
-      <v-dialog persistent v-model="isAdd" width="800">
-        <v-card class="rounded-lg elevation-5">
-          <v-card-title class="headline mb-2">Add Trip </v-card-title>
+      <v-dialog v-model="isAdd" max-width="500px">
+        <v-card>
+          <v-card-title class="headline mb-2">Add Trip</v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="newTrip.name"
-              label="Name"
-              required
-            ></v-text-field>
-
-            <v-text-field type="date"
-              v-model="newTrip.startDate"
-              label="Start Date"
-
-            ></v-text-field>
-            <v-text-field
-            type="date"
-              v-model="newTrip.endDate"
-              label="End date"
-
-            ></v-text-field>
-
-            <v-textarea
-              v-model="newTrip.description"
-              label="Description"
-            ></v-textarea>
+            <v-text-field v-model="newTrip.name" label="Name" required></v-text-field>
+            <v-row>
+              <v-col cols="6">
+                <v-text-field v-model="newTrip.startDate" label="Start Date" type="date"></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field v-model="newTrip.endDate" label="End Date" type="date"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea v-model="newTrip.description" label="Description"></v-textarea>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn variant="flat" color="secondary" @click="closeAdd()"
-              >Close</v-btn
-            >
-            <v-btn variant="flat" color="primary" @click="addTrip()"
-              >Add Trip</v-btn
-            >
+            <v-btn color="secondary" @click="closeAdd">Close</v-btn>
+            <v-btn color="primary" @click="addTrip">Add Trip</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-      <v-snackbar v-model="snackbar.value" rounded="pill">
-        {{ snackbar.text }}
 
-        <template v-slot:actions>
-          <v-btn
-            :color="snackbar.color"
-            variant="text"
-            @click="closeSnackBar()"
-          >
-            Close
-          </v-btn>
+      <v-snackbar v-model="snackbar.value" :color="snackbar.color" :timeout="3000">
+        {{ snackbar.text }}
+        <template v-slot:action="{ attrs }">
+          <v-btn text v-bind="attrs" @click="closeSnackBar">Close</v-btn>
         </template>
       </v-snackbar>
     </div>
